a3c295680a29d92ab02026ac39ac106c0141d52f
kovnerd <kovnerd@gmail.com>
2016-12-19 01:00:43 -0500

    fixed previous neighbor periodic indexing with %

--- metropolis_calculator.c
+++ metropolis_calculator.c
@@ -3,17 +3,16 @@
 #include <time.h>
 
 int main(void)
-
 {
 	
 	int N_points = 8;
 	double xs[N];
-	double propagator[N], energy[N - 1], energy_error[N];
+	double propagator[N], energy[N], energy_error[N];
 	gsl_rng *r = gsl_rng_alloc(gsl_rng_taus2);
 	unsigned long seed = ((unsigned long) time(NULL));
 	gsl_rng_set(r, seed);
 	int n = 0;
-	FILE *fp = fopen("harmonic_oscillator_metropolis.txt", "w");
+	FILE *fp = fopen("harmonic_oscillator_metropolis_x3x3.txt", "w");
 	
 	for(int i = 0; i < N; i++)
 	{
@@ -26,9 +25,7 @@ int main(void)
 	accumulateStatistics(xs, energy, propagator, energy_error, r);
 	
 	for(int i = 0; i < N_points; i++) 
-	{
-			fprintf(fp, "%f %f %f %f\n", (n++)*LATTICE_SPACING, propagator[i], energy[i]/LATTICE_SPACING, energy_error[i]/(LATTICE_SPACING));
-	}
+		fprintf(fp, "%f %f %f %f\n", (n++)*LATTICE_SPACING, propagator[i], energy[i], energy_error[i]);
 	gsl_rng_free(r);
 	fclose(fp);
 	
--- metropolis_path_sampling.c
+++ metropolis_path_sampling.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include "matrixmem.c"
-#define TIME 10
 #define N 20
 #define LATTICE_SPACING 0.5 // T/N
 #define MASS 1.
@@ -24,10 +23,9 @@ void metropolis(double *x, double epsilon, gsl_rng *r)
 	{
 		double oldX = x[i];
 		double oldS = S(x, i);
-		x[i] += epsilon * (2 * gsl_rng_uniform(r) - 1.);// generates random number between -epsilon and epsilon
+		x[i] += (2 * gsl_rng_uniform(r) - 1.)*epsilon;// generates random number between -epsilon and epsilon
 		double deltaS = S(x, i) - oldS;
-		double prob = exp(-deltaS);
-		if(prob < gsl_rng_uniform(r))
+		if(deltaS > 0 && exp(-deltaS) < gsl_rng_uniform(r))
 			x[i] = oldX;
 	}
 }
@@ -37,33 +35,33 @@ double G(double *xs, int n)
 	double x_prod = 0.;
 	for(int i = 0; i < N; i++)
 	{
-		x_prod += xs[(i + n)%N] * xs[i]; 
-		//x_prod += xs[(i + n)%N]*xs[(i + n)%N]*xs[(i + n)%N] * xs[i]*xs[i]*xs[i];
+		//x_prod += xs[(i + n)%N] * xs[i]; 
+		x_prod += xs[(i + n)%N]*xs[(i + n)%N]*xs[(i + n)%N] * xs[i]*xs[i]*xs[i];
 	}
 	
-	return x_prod/(double)N;
+	return x_prod/N;
 }
 
 double potential(double x)
 {
-	return 0.5*MASS*x*x;
+	return MASS*x*x/2;
 }
 
 double S(double* xs, int i)
 {
-	int iNext = (i+1)%N;
-	int iPrev = (i-1)%N;
-	return LATTICE_SPACING * potential(xs[i]) + xs[i]*(xs[i]-xs[iNext]-xs[iPrev])/LATTICE_SPACING;
+	int iNext = (i+1) % N;
+	int iPrev = (i-1+N) % N;
+	return LATTICE_SPACING*potential(xs[i]) + xs[i]*(xs[i]-xs[iNext]-xs[iPrev])/LATTICE_SPACING;
 }
 
 void bootstrap_G(double** Gs, double** bootstrap, int n_configs, gsl_rng *r)
 {
-	int alpha = 0;
-	for(int i = 0; i < n_configs; i++)
+	int rand = 0;
+	for(int alpha = 0; alpha < n_configs; alpha++)
 	{
-		alpha = (int) (gsl_rng_uniform(r)*(n_configs));//choose random config from Gs
+		rand = (int) (gsl_rng_uniform(r)*(n_configs));//choose random config from Gs
 		for(int n = 0; n < N; n++)//append random config to bootstrap
-			bootstrap[i][n] = Gs[alpha][n];
+			bootstrap[alpha][n] = Gs[rand][n];
 	}
 }
 
@@ -73,9 +71,7 @@ void Gs_average(double** Gs, double* avG, int n_configs)
 	{
 		avG[n] = 0;
 		for(int alpha = 0; alpha < n_configs; alpha++)
-		{
 			avG[n] += Gs[alpha][n];
-		}
 		avG[n] /= n_configs;
 	}
 }
@@ -102,24 +98,22 @@ void accumulateStatistics(double *xs, double *energy, double *avG, double *energ
 {	
 	int N_configs = 1000; //N_cf
 	int N_cor = 20; 
-	int bin_size = 20;
+	//int bin_size = 20;
 	int N_error = 100;
 	
-	int therm_steps = 7*N_cor;
+	int therm_steps = 5*N_cor;
 	double epsilon = 1.4;
 	
 	double **Gs = matrix_allocate_double(N_configs, N);
 	double **Gs_bootstrap = matrix_allocate_double(N_configs, N);
 	double *G_temp = malloc((unsigned long)N*sizeof(double));
-	double **Gs_bins = matrix_allocate_double(N_configs/bin_size, N);
-	double **energy_bootstrap = matrix_allocate_double(N - 1, N_error);
+	//double **Gs_bins = matrix_allocate_double(N_configs/bin_size, N);
+	double **energy_bootstrap = matrix_allocate_double(N, N_error);
 	double *temp = malloc((unsigned long)N_error*sizeof(double));
-	for(int n_err = 0; n_err < N_error; n_err++)
-	{
 		//initialize pointers on heap memory
 		for(int i = 0; i < N; i++)
 		{
-			xs[i] = 0.;
+			xs[i] = 0;
 			G_temp[i] = 0.;
 		}
 	for(int alpha = 0; alpha < N_configs; alpha++)
@@ -128,10 +122,11 @@ void accumulateStatistics(double *xs, double *energy, double *avG, double *energ
 			Gs[alpha][n] = 0.;
 			Gs_bootstrap[alpha][n] = 0.;
 		}
-	for(int b = 0; b < N_configs/bin_size; b++)
+	/*for(int b = 0; b < N_configs/bin_size; b++)
 		for(int n = 0; n < N; n++)
-			Gs_bins[b][n] = 0.;
-	for(int n = 0; n < N - 1; n++)
+			Gs_bins[b][n] = 0.;*/
+	for(int n = 0; n < N; n++)
+		for(int n_err = 0; n_err < N_error; n_err++)
 			energy_bootstrap[n][n_err] = 0.;
 			
 			
@@ -146,34 +141,30 @@ void accumulateStatistics(double *xs, double *energy, double *avG, double *energ
 	}
 	Gs_average(Gs, avG, N_configs);//compute mc average of propagator G_n
 	
-	for(int n = 0; n < N - 1; n++)
-		energy_bootstrap[n][n_err] = log(fabs(avG[n]/avG[n + 1]));
-	}
 	
-	/*
 	for(int n_err = 0; n_err < N_error; n_err++)//energy error analysis
 	{
 		bootstrap_G(Gs, Gs_bootstrap, N_configs, r);//generate bootstrap copies of Gs
-		bin_G(Gs_bootstrap, Gs_bins, N_configs, bin_size);//bin Gs bootstrap copies 
-		Gs_average(Gs_bins, G_temp, N_configs/bin_size);//get propagator values from binned bootstrap copy of Gs
-		
+		//bin_G(Gs_bootstrap, Gs_bins, N_configs, bin_size);//bin Gs bootstrap copies
+		Gs_average(Gs_bootstrap, G_temp, N_configs);//get propagator values from binned bootstrap copy of Gs
 		for(int n = 0; n < N; n++)
-			energy_bootstrap[n][n_err] = (n < N - 1) ? log(fabs(G_temp[n]/G_temp[n + 1])) :  log(fabs(G_temp[n]/G_temp[0]));//generate sample of energies
-	}*/
+			energy_bootstrap[n][n_err] = log(fabs(G_temp[n]/G_temp[(n + 1)%N]));//generate sample of energies
+	}
 	
-	for(int n = 0; n < N - 1; n++)
+	for(int n = 0; n < N; n++)
 	{
+		
 		for(int n_err  = 0; n_err < N_error; n_err++)
 			temp[n_err] = energy_bootstrap[n][n_err];
-		energy[n] = log(fabs(avG[n]/avG[n + 1]));
+		energy[n] = log(fabs(avG[n]/avG[(n + 1)%N]))/LATTICE_SPACING;
 		energy_error[n] = gsl_stats_sd(temp, 1, (size_t) N_error);
-		printf("%f %f %f %f\n", n * LATTICE_SPACING, avG[n], energy[n]/LATTICE_SPACING, energy_error[n]/(LATTICE_SPACING));
+		printf("%f %f %f %f\n", n * LATTICE_SPACING, avG[n], energy[n], energy_error[n]);
 	}
 	
 	
 	matrix_free_double(Gs);
 	matrix_free_double(Gs_bootstrap);
-	matrix_free_double(Gs_bins);	
+	//matrix_free_double(Gs_bins);	
 	free(temp);
 	free(G_temp);
 	matrix_free_double(energy_bootstrap);
